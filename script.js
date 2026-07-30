79128a497d4303a232eb1a6d40f0fe330e06fe13
ahmedmadkour3 <ahmed333madkour@gmail.com>
2026-07-30 16:45:29 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 const typing = document.getElementById("typing");
 const days = document.getElementById("days");
+const msg = "أنتِ أجمل شيء حدث في حياتي ❤️";
+let i = 0;
+
+function t() {
+  if (i < msg.length) {
+    typing.innerHTML += msg[i++];
+    setTimeout(t, 80);
+  }
+}
+
+t();
